48da504f02e6389d93aa8fa93eca689d600a849e
Kosuke Kusano <cocuh.kk@gmail.com>
2016-09-29 13:48:56 +0900

    refactoring

--- chainer_utils/evaluators.py
+++ chainer_utils/evaluators.py
@@ -19,39 +19,44 @@ class HorizontalParallelEvaluator(object):
         self._input_queue = mp.Queue(self.device_num)
         self._output_queue = mp.Queue()
 
-        self._iter_process = None
-        self._eval_processes = []
+        self._iter_worker = None
+        self._eval_workers = []
 
     def run(self, model, iterator):
-        self._iter_process = mp.Process(target=_iter_worker,
-            args=(iterator, self._input_queue, self.device_num))
-        self._iter_process.start()
+        self._iter_worker = mp.Process(target=_iter_worker, args=(iterator, self._input_queue, self.device_num))
+        self._iter_worker.start()
 
-        self._eval_processes = [
+        self._eval_workers = [
             mp.Process(
-                target=_EvalWorker(worker_id, device_id, self._input_queue, self._output_queue, model),
+                target=EvalWorker(worker_id, device_id, self._input_queue, self._output_queue, model),
             )
             for worker_id, device_id in enumerate(self.devices.values())
             ]
 
-        for workers in self._eval_processes:
+        for workers in self._eval_workers:
             workers.start()
 
         try:
-            for workers in self._eval_processes:
-                workers.join()
-            self._iter_process.join()
+            self._join_workers()
         except KeyboardInterrupt:
-            for workers in self._eval_processes:
-                workers.terminate()
-            self._iter_process.terminate()
+            self._terminate_workers()
         return self._collect_result()
+
+    def _join_workers(self):
+        for workers in self._eval_workers:
+            workers.join()
+        self._iter_worker.join()
 
     def _collect_result(self):
         res = []
         while not self._output_queue.empty():
             res.append(self._output_queue.get())
         return res
+
+    def _terminate_workers(self):
+        for workers in self._eval_workers:
+            workers.terminate()
+        self._iter_worker.terminate()
 
 
 def _iter_worker(iterator, input_queue, device_num):
@@ -67,13 +72,7 @@ def _iter_worker(iterator, input_queue, device_num):
         input_queue.put(None)
 
 
-def _pop_and_alloc_item(arr, to_xxx):
-    if arr is None:
-        return None
-    return to_xxx(arr)
-
-
-class _EvalWorker(object):
+class EvalWorker(object):
     def __init__(self, worker_id, device_id, input_queue, output_queue, model):
         self.worker_id = worker_id
         self.device_id = device_id
@@ -94,7 +93,6 @@ class _EvalWorker(object):
     def _prefetch(self):
         while True:
             data = self.input_queue.get()
-            print('prefetch', self.worker_id, data[0][-1] + 1)
             if data is None:
                 self.prefetch_queue.put(None)
                 break
@@ -107,8 +105,6 @@ class _EvalWorker(object):
                 self.prefetch_queue.put(data)
 
     def _to_xxx(self, arr):
-        if arr is None:
-            return None
         if self.device_id < 0:
             if isinstance(arr, np.ndarray):
                 return cuda.to_cpu(arr)
@@ -120,11 +116,17 @@ class _EvalWorker(object):
             else:
                 return arr
 
+    def _to_cpu(self, arr):
+        if isinstance(arr, np.ndarray):
+            arr = cuda.to_cpu(arr)
+        return arr
+
     def __call__(self):
         self.prefetch_thread.start()
         for data in iter(self.prefetch_queue.get, None):
+            data = self._to_xxx(data)
             res = self.model(*data)
-            self.output_queue.put((self._to_xxx(res),))
+            self.output_queue.put((self._to_cpu(res),))
             del data
             self.prefetch_queue.task_done()
 
@@ -137,8 +139,11 @@ def main():
         'first': -1,
         'second': -1,
     })
-    model = lambda x, y: print('start', x[-1] + 1) or __import__('time').sleep(3) or print('end',
-        x[-1] + 1) or x.sum() + y.sum()
+    model = lambda x, y: \
+        print('start', x[-1] + 1) or \
+        __import__('time').sleep(3) or \
+        print('end', x[-1] + 1) or \
+        x.sum() + y.sum()
 
     def iterator():
         import numpy as np
